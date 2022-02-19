2a97de2b7e353a73d3b07e395970048668245c75
Alexander <caminante.msk@gmail.com>
2022-02-19 22:52:11 +0300

    added obras, opinions sections

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import './styles/index.scss';
 
 import './components/header/header.js';
 import './components/services/services.js';
+import './components/obras/obras.js';
 import './components/opinions/opinions.js';
 
