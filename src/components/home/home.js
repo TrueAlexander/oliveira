55351d71dc5bcfa83dc7ec13a504348e38238bdb
Alexander <caminante.msk@gmail.com>
2022-09-19 17:56:29 -0300

    experim

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -27,11 +27,15 @@ if (visualViewport.width > 1050) {
 
       if (button.classList.contains('opened')) {
         buttonOpened()
+        box.style.height = 'auto'
+        box.style.paddingTop = '150px'
         description.insertAdjacentHTML('beforeend', `
             <p class = "home__description animate__animated animate__fadeIn animate__slower"> A Oliveira Serviços de Engenharia nasceu da necessidade de oferecer um serviço técnico, de qualidade, com garantia e com acompanhamento desde o início até o fim da intervenção. Atuamos com excelência nas áreas de laudos extrajudiciais e autovistoria, com nossa equipe trabalhamos com dinamismo e eficiência na manutenção de condomínios e ainda oferecemos um grande portifólio de atividades. Com apoio da nossa equipe atuamos na manutenção de fachadas, adequação de barriletes de acordo com a ABNT NBR 5626 e Impermeabilização. Atendemos a todo o Rio de Janeiro com uma visão única que possibilita a todos os nossos clientes, parceiros e amigos ter segurança técnica, fundamentada nas normas vigentes atuais, e segurança financeira, tudo para que o resultado seja alcançado.</p>
               `)
       } else {
         buttonClosed()
+        box.style.height = '100vh'
+        box.style.paddingTop = '125px'
         description.insertAdjacentHTML('beforeend', `
             <p class = "home__description animate__animated animate__fadeIn animate__slower"> A Oliveira Serviços de Engenharia nasceu da necessidade de oferecer um serviço técnico, de qualidade, com garantia e com acompanhamento desde o início até o fim da intervenção...</p>
               `) 
